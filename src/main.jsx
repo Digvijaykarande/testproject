7b0d15ab6ec44fbdec4a79545d5fc366b7971e35
Digvijaykarande <karandedigvijay2@gmail.com>
2026-03-20 09:25:13 +0530

    hii

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import{BrowserRouter} from "react-router-dom"
 import './index.css'
-import App from './App.jsx'
-import { FirebaseProvider } from './context/Rirebase.jsx' 
+import App from './App.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <FirebaseProvider>
     <App />
-    </FirebaseProvider>
     </BrowserRouter>
   </StrictMode>,
 )
